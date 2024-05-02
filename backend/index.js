59b8cb301aef23794cd8713109b2242cead7ebbe
Muzahidul Islam <muzahidul.islam@Optimizely.com>
2024-05-02 14:53:45 +0600

    Google generative AI started working

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const { createServer } = require('node:http');
+const getCompletion = require('./chat_completion.js');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
+const server =  createServer( async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  const completion = await getCompletion();
+  res.end(completion);
 });
 
 server.listen(port, hostname, () => {
