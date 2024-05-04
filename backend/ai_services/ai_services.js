0ce36223205bcbbbbfca616a1ac3bdf88032b2fa
Muzahidul Islam <muzahidul.islam@Optimizely.com>
2024-05-04 17:41:36 +0600

    image comparisn added

--- backend/ai_services/ai_services.js
+++ backend/ai_services/ai_services.js
@@ -1,10 +1,26 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const OpenAI = require('openai');
+const fs = require('fs');
+const path = require('path');
+const scriptDir = path.dirname(__filename);
 
 class AIService {  
   async textCompletion(prompt) {
     throw new Error('Method not implemented.');
   }
+
+  async imageCompletion(prompt) {
+    throw new Error('Method not implemented.');
+  }
+}
+
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType
+    },
+  };
 }
 
 class GoogleAIService extends AIService {
@@ -12,9 +28,29 @@ class GoogleAIService extends AIService {
       super();
       this.genAI = new GoogleGenerativeAI(apiKey);
     }
-  
+
+    async imageCompletion(prompt) {
+      // const prompt = "What's different between these pictures?";
+      const model = this.genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+      console.log('prompt', prompt);
+      console.log(process.cwd());
+      const imagePath1 = path.join(scriptDir, 'image1.png');
+      const imagePath2 = path.join(scriptDir, 'image2.jpeg');
+
+      const imageParts = [
+        fileToGenerativePart(imagePath1, "image/png"),
+        fileToGenerativePart(imagePath2, "image/jpeg"),
+      ];
+      console.log('imageParts', imageParts);
+      const result = await model.generateContent([prompt, ...imageParts]);
+      const response = await result.response;
+      const text = response.text();
+      return text;
+    }
+
     async textCompletion(prompt) {
       console.log('prompt', prompt);
+      // gemini-pro-vision
       const model = this.genAI.getGenerativeModel({ model: "gemini-pro"});
       console.log('model', model);
       const result = await model.generateContent(prompt);
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const { createServer } = require('node:http');
-const getCompletion = require('./chat_completion.js');
 const { AIService, getAIService } = require('./ai_services/ai_services');
 
 const hostname = '127.0.0.1';
@@ -10,8 +9,10 @@ const server =  createServer( async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   const aiService = getAIService(process.env.GOOGLE_API_KEY, 'google');
-  const completion = await aiService.textCompletion("Write a story about a magic backpack.");
-  res.end(completion);
+  // const completion = await aiService.textCompletion("Write a story about a magic backpack.");
+  // res.end(completion);
+  const completion2 = await aiService.imageCompletion("What's different between these pictures?");
+  res.end(completion2);
 });
 
 server.listen(port, hostname, () => {
