1e7dac5d467a99653e03a83a0ee49d43c1348ccf
Muzahidul Islam <muzahidul.islam@Optimizely.com>
2024-05-30 11:38:20 +0600

    upload image route seperated

--- backend/controllers/uploadController.js
+++ backend/controllers/uploadController.js
@@ -1,1 +1,19 @@
-const multer  = require('multer')
+// const multer  = require('multer')
+
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, path.dirname(__filename) + '/uploads');
+//     },
+//     filename: function(req, file, cb) {
+//         const tempImage = 'sample.png'; // default extension
+//         cb(null, file.fieldname + '-' + tempImage);
+//     }
+// });
+
+// const upload = multer({ storage: storage});
+
+// async function handleUpload(req, res) {
+//     console.log('Received image:', req.file);
+//     const completion = { image: req.file.path };
+//     res.json(completion);
+// };
--- backend/index.js
+++ backend/index.js
@@ -7,44 +7,47 @@ const path = require('path')
 const { AIService, getAIService } = require('./src/ai_services');
 const { isValidYoutubeUrl } = require('./utils/utils.js');
 const contentCompletionRoute = require('./routes/ContentCompletionRoute');
+const uploadRoute = require('./routes/uploadRoute');
 const app = express();
 const port = 3999;
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, path.dirname(__filename) + '/uploads');
-  },
-  filename: function(req, file, cb) {
-    const tempImage = 'sample.png'; // default extension
-    cb(null, file.fieldname + '-' + tempImage);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//     cb(null, path.dirname(__filename) + '/uploads');
+//   },
+//   filename: function(req, file, cb) {
+//     const tempImage = 'sample.png'; // default extension
+//     cb(null, file.fieldname + '-' + tempImage);
+//   }
+// });
 
-const upload = multer({ storage: storage});
+// const upload = multer({ storage: storage});
 app.use(cors())
 app.use(express.json()); // for parsing application/json
 app.use(contentCompletionRoute);
-const aiService = getAIService(process.env.GOOGLE_API_KEY, 'google');
+app.use(uploadRoute);
 
-app.post('/text-completion', async (req, res) => {
-  console.log('text-completion', req.body);
-  const text = req.body.text;
-  const completion = await aiService.textCompletion(text);
-  console.log('completion', completion);
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.write(`data: ${JSON.stringify(completion)}\n\n`);
-  res.end();
-});
+// const aiService = getAIService(process.env.GOOGLE_API_KEY, 'google');
 
-app.post('/upload', upload.single('image'), async (req, res) => {
-  console.log('Received image:', req.file);
-  const completion = { image: req.file.path };
-  res.json(completion);
-});
+// app.post('/text-completion', async (req, res) => {
+//   console.log('text-completion', req.body);
+//   const text = req.body.text;
+//   const completion = await aiService.textCompletion(text);
+//   console.log('completion', completion);
+//   res.setHeader('Content-Type', 'text/event-stream');
+//   res.write(`data: ${JSON.stringify(completion)}\n\n`);
+//   res.end();
+// });
 
-app.get('/', (req, res) => {
-  res.send('Hi ther! This is the from AI sandbox server');
-})
+// app.post('/upload', upload.single('image'), async (req, res) => {
+//   console.log('Received image:', req.file);
+//   const completion = { image: req.file.path };
+//   res.json(completion);
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Hi ther! This is the from AI sandbox server');
+// })
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
