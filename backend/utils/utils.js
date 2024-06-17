de0b43376159b77c32eae229d1e01b62e38c87c9
Muzahidul Islam <muzahidul.islam@Optimizely.com>
2024-06-17 06:21:02 +0600

    clean up

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,9 +1,14 @@
 
+/**
+ * Checks if a given URL is a valid YouTube URL.
+ *
+ * @param {string} url - The URL to be checked.
+ * @returns {boolean} - Returns true if the URL is a valid YouTube URL, otherwise returns false.
+ */
  function isValidYoutubeUrl(url) {
     // Regular expression to match a valid YouTube URL
     const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+/;
     return youtubeRegex.test(url);
 }
-
 
 module.exports = { isValidYoutubeUrl };
