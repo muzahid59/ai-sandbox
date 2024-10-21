91bc933d94d6cd6d8db5046388d4d784acc85335
Muzahidul Islam <muzahidul.islam@Optimizely.com>
2024-10-21 10:49:39 +0600

    docker build added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3999;
+const port = 5000;
 const cors = require('cors');
 const routes = require('./routes');
 
